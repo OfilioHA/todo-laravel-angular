7ac450b12f4cfd68bad9df5be4720677a119c27c
OfilioHA <herdocia20@gmail.com>
2024-06-27 02:11:57 -0600

    feat: :sparkles: Set main config of app

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TasksListComponent } from './tasks/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TasksListComponent },
+  { path: 'create', component: TasksListComponent },
+  { path: 'edit/:id', component: TasksListComponent },
+];
