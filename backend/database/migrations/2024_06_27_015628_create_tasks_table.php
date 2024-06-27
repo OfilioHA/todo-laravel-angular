0042122c943a024707fb27ac8f39e6881e7d7ac1
OfilioHA <herdocia20@gmail.com>
2024-06-26 21:22:15 -0600

    feat: :sparkles: Setting Task migration

--- backend/database/migrations/2024_06_27_015628_create_tasks_table.php
+++ backend/database/migrations/2024_06_27_015628_create_tasks_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->enum('priority', Config::get('app.priorities'));
+            $table->text('description');
+            $table->timestamp('finished_at')->nullable();
             $table->timestamps();
         });
     }
